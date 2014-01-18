055b5636d38db6308a60aebbe7e94c9b167ca87b
Luan fonseca de farias <luanfonceca@gmail.com>
2014-01-18 14:34:33 -0300

    Adicionando o this.py original..

---
+++ this.py
@@ -0,0 +1,21 @@
+print """The Zen of Python, by Tim Peters
+
+Aprumado é melhor que entrouxado. Beautiful is better than ugly.
+Na fuça é melhor que intocado. Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Nestante é melhor que de hoje a quinze. Now is better than never.
+Although never is often better than *right* now.
+Se o muído for caixão de contar, é bronca. If the implementation is hard to explain, it's a bad idea.
+Se o muído for fichinha de contar, é só o mi. If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
