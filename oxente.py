2e92f3c12e1576ec90414ec5e829c647c5c8a1e6
Luan Fonseca de Farias <luanfonceca@gmail.com>
2014-01-19 16:40:13 -0200

    Update oxente.py

--- oxente.py
+++ oxente.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-print """The Zen of Python, por Tim Peters e bulido por Luan Fonseca
+print """O Cordel do Python, por Tim Peters e bulido por Luan Fonseca
 
 Aprumado é melhor que entrouxado.
 Na fuça é melhor que intocado.
@@ -17,7 +17,7 @@ Unless explicitly silencwed.
 In the face of ambiguity, refuse the temptation to guess.
 There should be one-- and preferably only one --obvious way to do it.
 Although that way may not be obvious at first unless you're Dutch.
-Nestante é melhor que de hoje a quinze. Now is better than never.
+Nestante é melhor que de hoje a quinze.
 Although never is often better than *right* now.
 Se o muído for caixão de contar, é bronca.
 Se o muído for fichinha de contar, é só o mi.
