2d1152adf6fa8adb9366e698d485cdf812e8b434
Luan fonseca de farias <luanfonceca@gmail.com>
2014-05-03 15:54:29 -0300

    Criando o setup.py e movendo o oxente para um pacote

--- oxente/__init__.py
+++ oxente/__init__.py
@@ -10,15 +10,15 @@ Complex is better than complicated.
 Flat is better than nested.
 Sparse is better than dense.
 Readability counts.
-Special cases aren't special enough to break the rules.
+Special cases arent special enough to break the rules.
 Although practicality beats purity.
 Errors should never pass silently.
 Unless explicitly silencwed.
 In the face of ambiguity, refuse the temptation to guess.
 There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
+Although that way may not be obvious at first unless youre Dutch.
 Nestante é melhor que de hoje a quinze. Now is better than never.
 Although never is often better than *right* now.
 Se o muído for caixão de contar, é bronca.
 Se o muído for fichinha de contar, é só o mi.
-Namespaces are one honking great idea -- let's do more of those!"""
+Namespaces are one honking great idea -- lets do more of those!"""
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup
+
+try:
+    long_description = file('README.rst').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name='oxente',
+    version='127.0.0.1',
+    url='https://github.com/luanfonceca/oxente',
+    license='MIT',
+    description=u'Tradução do módulo "this" do python para PT-BR-NE (Português brasileiro nordestino)',
+    long_description=long_description,
+    author='Luan Fonseca',
+    author_email='luanfonceca@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    packages=['oxente'],
+    install_requires=['setuptools'],
+)
