6f60ed167ad69c6b1b905b5621ae749510533623
Luan fonseca de farias <luanfonceca@gmail.com>
2014-11-06 11:23:27 -0300

    Adicionando alterações no __init__.py

--- oxente/__init__.py
+++ oxente/__init__.py
@@ -3,11 +3,11 @@
 
 print """O Cordel do Python, por Tim Peters e bulido por Luan Fonseca
 
-Aprumado é melhor que entrouxado.
-Na fuça é melhor que intocado.
+Aprumado é melhor do que troncho.
+Nas fuça é melhor que intocado.
 Sem arrudeio é melhor que de rosca.
 De rosca é melhor que um nó cego.
-Daqui prali é melhor que esbuguelado.
+Linherim é melhor que esbuguelado.
 Espaiado é melhor que chei de guéri-guéri.
 Facim de lê é bão.
 Esses casos cheio de rififi num vale o peido duma gaja mijada.
@@ -18,8 +18,8 @@ Se tem muita gente e alguém peidou, melhor cheirar calado do que acusar em vão
 Deve ter um jeito -- e sem muído -- de fazer uma resenha.
 Mais esse jeitim pode num tá na frente da sua venta de início, a num ser que você seja das zoropa.
 Pode ser que num teje claro, mai num seje abestado.
-Nestante é melhor que de hoje a quinze.
-Mas de hoje a quinze é melhor que *nas carrera*.
+Nes'tante é melhor que de hoje a quinze.
+Mas de hoje a quinze é melhor que *nas carrêra*.
 Se o muído for caixão de contar, é bronca.
-Se o muído for fichinha de contar, é só o mi.
-Namespaces é uma ideia paidégua -- bó matutar mardéssa."""
+Se der pra contar o muído ligêro, é só o mi.
+Apartar os nomes é uma ideia pai d'égua -- bó matutar mardéssa."""
